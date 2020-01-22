0bf2467ee4f19149480e73dcf96d1c5c0ac6a40b
Rand al'Thor <alexanderhogle@gmail.com>
2020-01-22 17:01:00 -0600

    working on inquirer functions

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
-    }
-]).then(function(data) {
-    
-    this.name = JSON.stringify(data.name);
-    
-    
-    
-    
+//Classes
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
 
-    console.log("Success!");
-})
+
+//Node Modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const userInput = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter manager name:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter manager e-mail:",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Enter manager office number:"
+  },
+  {
+    type: "list",
+    name: "addEmployee",
+    message: "Add employees?",
+    choices: ["Yes", "No"]
+  }
+]
+
+function init() {
+  inquirer.prompt(userInput)
+  }
+
+init();
