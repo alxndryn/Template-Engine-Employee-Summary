9ceeaaa006ef9ec6c7007f3fc44ca564dbe92543
Rand al'Thor <alexanderhogle@gmail.com>
2020-04-27 19:04:51 -0500

    prettify code and adds sample gif

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 
-
 //Node Modules
 const inquirer = require("inquirer");
 const fs = require("fs");
@@ -13,73 +12,83 @@ const userInput = [
   {
     type: "input",
     name: "name",
-    message: "Enter manager name:"
+    message: "Enter manager name:",
   },
   {
     type: "input",
     name: "email",
-    message: "Enter manager e-mail:"
+    message: "Enter manager e-mail:",
   },
   {
     type: "input",
     name: "officeNumber",
-    message: "Enter manager office number:"
+    message: "Enter manager office number:",
   },
   {
     type: "list",
     name: "addEmployee",
     message: "Add employees?",
-    choices: ["Yes", "No"]
-  }
+    choices: ["Yes", "No"],
+  },
 ];
 
 const teamInput = [
   {
     type: "input",
     name: "name",
-    message: "Enter employee name:"
+    message: "Enter employee name:",
   },
   {
     type: "input",
     name: "email",
-    message: "Enter employee email:"
+    message: "Enter employee email:",
   },
   {
     type: "list",
     name: "role",
-    choices: ["Engineer", "Intern"]
+    choices: ["Engineer", "Intern"],
   },
   {
-    when: answer => {
-      return answer.role == "Engineer"
+    when: (answer) => {
+      return answer.role == "Engineer";
     },
     type: "input",
     name: "github",
-    message: "Enter Github username:"
+    message: "Enter Github username:",
   },
   {
-    when: answer => {
-      return answer.role == "Intern"
+    when: (answer) => {
+      return answer.role == "Intern";
     },
     type: "input",
     name: "school",
-    message: "Enter the name of your school:"
+    message: "Enter the name of your school:",
   },
   {
     type: "list",
     name: "addEmployee",
     message: "Add employees?",
-    choices: ["Yes", "No"]
-  }
-]
+    choices: ["Yes", "No"],
+  },
+];
 
 //Adds additional team members
 function addEmployee() {
-  inquirer.prompt(teamInput).then(answer => {
+  inquirer.prompt(teamInput).then((answer) => {
     if (answer.role == "Engineer") {
-      var newEmployee = new Engineer(answer.name, teamArr.length + 1, answer.email, answer.github);
+      var newEmployee = new Engineer(
+        answer.name,
+        teamArr.length + 1,
+        answer.email,
+        answer.github
+      );
     } else {
-      var newEmployee = new Intern(answer.name, teamArr.length + 1, answer.email, answer.school);
+      var newEmployee = new Intern(
+        answer.name,
+        teamArr.length + 1,
+        answer.email,
+        answer.school
+      );
     }
     teamArr.push(newEmployee);
     console.log(teamArr);
@@ -88,68 +97,87 @@ function addEmployee() {
     } else {
       buildPage();
     }
-  })
+  });
 }
 
-
-//Builds HTML page (unfinished)
+//Builds HTML page
 function buildPage() {
-  let newFile = fs.readFileSync("./templates/main.html")
+  let newFile = fs.readFileSync("./templates/main.html");
   fs.writeFileSync("./output/team.html", newFile, function (err) {
-      if (err) throw err;
-  })
+    if (err) throw err;
+  });
 
   console.log("Main page generated!");
 
   for (employee of teamArr) {
-      if (employee.getRole() == "Manager") {
-          buildHtmlCard("manager", employee.getName(), employee.getId(), employee.getEmail(), "Office: " + employee.getOfficeNumber());
-      } else if (employee.getRole() == "Engineer") {
-          buildHtmlCard("engineer", employee.getName(), employee.getId(), employee.getEmail(), "Github: " + employee.getGithub());
-      } else if (employee.getRole() == "Intern") {
-          buildHtmlCard("intern", employee.getName(), employee.getId(), employee.getEmail(), "School: " + employee.getSchool());
-      }
+    if (employee.getRole() == "Manager") {
+      buildHtmlCard(
+        "manager",
+        employee.getName(),
+        employee.getId(),
+        employee.getEmail(),
+        "Office: " + employee.getOfficeNumber()
+      );
+    } else if (employee.getRole() == "Engineer") {
+      buildHtmlCard(
+        "engineer",
+        employee.getName(),
+        employee.getId(),
+        employee.getEmail(),
+        "Github: " + employee.getGithub()
+      );
+    } else if (employee.getRole() == "Intern") {
+      buildHtmlCard(
+        "intern",
+        employee.getName(),
+        employee.getId(),
+        employee.getEmail(),
+        "School: " + employee.getSchool()
+      );
+    }
   }
-  fs.appendFileSync("./output/team.html", "</div></section></body></html>", function (err) {
+  fs.appendFileSync(
+    "./output/team.html",
+    "</div></section></body></html>",
+    function (err) {
       if (err) throw err;
-  });
-  console.log("Added employees to page! Process finished.")
-
+    }
+  );
+  console.log("Added employees to page! Process finished.");
 }
 
 function buildHtmlCard(employeeType, name, id, email, propertyValue) {
-  let data = fs.readFileSync(`./templates/${employeeType}.html`, 'utf8')
+  let data = fs.readFileSync(`./templates/${employeeType}.html`, "utf8");
   data = data.replace("nameHere", name);
   data = data.replace("idHere", `ID: ${id}`);
-  data = data.replace("emailHere", `Email: <a href="mailto:${email}">${email}</a>`);
+  data = data.replace(
+    "emailHere",
+    `Email: <a href="mailto:${email}">${email}</a>`
+  );
   data = data.replace("propertyHere", propertyValue);
-  fs.appendFileSync("./output/team.html", data, err => { if (err) throw err; })
+  fs.appendFileSync("./output/team.html", data, (err) => {
+    if (err) throw err;
+  });
   console.log("Card appended");
 }
 
-
 //Run application
 function init() {
-  inquirer.prompt(userInput).then(answer => {
-    let teamManager = new Manager (answer.name, 1, answer.email, answer.officeNumber);
+  inquirer.prompt(userInput).then((answer) => {
+    let teamManager = new Manager(
+      answer.name,
+      1,
+      answer.email,
+      answer.officeNumber
+    );
     teamArr.push(teamManager);
     console.log(teamArr);
     if (answer.addEmployee === "Yes") {
       addEmployee();
     } else {
-     buildPage();
+      buildPage();
     }
-  })
-  }
+  });
+}
 
 init();
-
-//manager questions
-//employee questions
-//function for manager to build page(currently console.log) or add employees
-//function for employees to build page(currently console.log) or add more employees
-
-//create html page writing function
-//write html templates
-
-//Week 09 Activity 40
